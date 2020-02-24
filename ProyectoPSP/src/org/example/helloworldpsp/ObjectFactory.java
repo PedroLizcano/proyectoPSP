dcb99bf7ed45a6834fd3669f7d273276d7d07397
pedrosolano <60041086+pedrosolano@users.noreply.github.com>
2020-02-24 19:25:32 +0100

    24/02/2020

---
+++ ProyectoPSP/src/org/example/helloworldpsp/ObjectFactory.java
@@ -0,0 +1,64 @@
+
+package org.example.helloworldpsp;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the org.example.helloworldpsp package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.example.helloworldpsp
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Hello }
+     * 
+     */
+    public Hello createHello() {
+        return new Hello();
+    }
+
+    /**
+     * Create an instance of {@link HelloResponse }
+     * 
+     */
+    public HelloResponse createHelloResponse() {
+        return new HelloResponse();
+    }
+
+    /**
+     * Create an instance of {@link Goodbye }
+     * 
+     */
+    public Goodbye createGoodbye() {
+        return new Goodbye();
+    }
+
+    /**
+     * Create an instance of {@link GoodbyeResponse }
+     * 
+     */
+    public GoodbyeResponse createGoodbyeResponse() {
+        return new GoodbyeResponse();
+    }
+
+}
---
+++ ProyectoPSP/src/org/example/helloworldpsp/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/helloWorldPSP/")
+package org.example.helloworldpsp;
